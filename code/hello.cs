2ed9fe7e646e784ae15f3c2eca5b7d208be1f98b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-02-24 18:59:53 +0800

    Site updated: 2017-02-24 18:59:47

---
+++ code/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hello
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
