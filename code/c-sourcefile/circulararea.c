4aa2dcc14d4aaaf1e1524ecff183715f41a61b5e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-03-23 17:28:24 +0800

    Site updated: 2017-03-23 17:27:56

---
+++ code/c-sourcefile/circulararea.c
@@ -0,0 +1,8 @@
+// circulararea.c: Calculates the areas of circles.
+// Called by main() in circle.c
+
+double circularArea( double r ) // Definition of circularArea( ) begins
+{
+    const double pi = 3.1415926536; // Pi is a constant
+    return pi * r * r;
+}
